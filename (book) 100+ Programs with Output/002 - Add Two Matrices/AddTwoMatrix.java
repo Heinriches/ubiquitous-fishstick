bfef88ab39b7ad001cf44702b00f2993d39048fb
Heinriches <heydrich.institut@gmail.com>
2025-09-23 11:47:09 -0300

    Update AddTwoMatrix.java

--- (book) 100+ Programs with Output/002 - Add Two Matrices/AddTwoMatrix.java
+++ (book) 100+ Programs with Output/002 - Add Two Matrices/AddTwoMatrix.java
@@ -1,3 +1,15 @@
+/**
+ * This code adds two matrices, but you easily modify it to add any number
+ * of matrices. One approach is to create a 'Matrix' class. Whithin this class, 
+ * you would implement an 'add' method that sums two matrix objects. To add
+ * more than two matrices, simply call this 'add' method repeatedly inside a loop. 
+ */
+
+import java.util.Scanner;
+
+
 class AddTwoMatrix{
-	
+	public static void main(String[] args) {
+		
+	}
 }
