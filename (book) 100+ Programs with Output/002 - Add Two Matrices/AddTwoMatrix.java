be8c9274ad4409724ea33019a5e2a7e1e30f0856
Heinriches <heydrich.institut@gmail.com>
2025-09-23 12:37:06 -0300

    Update AddTwoMatrix.java

--- (book) 100+ Programs with Output/002 - Add Two Matrices/AddTwoMatrix.java
+++ (book) 100+ Programs with Output/002 - Add Two Matrices/AddTwoMatrix.java
@@ -9,55 +9,53 @@ import java.util.Scanner;
 
 class AddTwoMatrix{
 	public static void main(String[] args) {
-		public static void main(String[] args) {
 			
-			Scanner in = new Scanner(System.in);
-			
-			//	---	---	---	---	---	---	---	---	---
-			
-			System.out.println("Inform the number of rows and columns in your two matrices: ");
-			int m = in.nextInt();
-			int n = in. nextInt();
-			
-			int first [][]		= new int [m][n];
-			int second [][]		= new int [m][n];
-			int sum [][]			= new int [m][n];
-			
-			//	---	---	---	---	---	---	---	---	---
-			
-			System.out.println("Enter the elements of your firt matrix: ");
-			for(int c = 0; c < m ; c++) {
-				for( int d = 0; d < n; d++) {
-					first [c][d] = in.nextInt();
-				}
+		Scanner in = new Scanner(System.in);
+		
+		//	---	---	---	---	---	---	---	---	---
+		
+		System.out.println("Inform the number of rows and columns in your two matrices: ");
+		int m = in.nextInt();
+		int n = in. nextInt();
+		
+		int first [][]		= new int [m][n];
+		int second [][]		= new int [m][n];
+		int sum [][]			= new int [m][n];
+		
+		//	---	---	---	---	---	---	---	---	---
+		
+		System.out.println("Enter the elements of your firt matrix: ");
+		for(int c = 0; c < m ; c++) {
+			for( int d = 0; d < n; d++) {
+				first [c][d] = in.nextInt();
 			}
-			
-			System.out.println("Enter the elements of your second matrix: ");
-			for(int c = 0; c < m; c++) {
-				for(int d = 0; d < n; d++) {
-					second[c][d] = in.nextInt();
-				}
-			}
-			
-			//	---	---	---	---	---	---	---	---	---
-			
-			for(int c = 0; c < m; c++) {
-				for(int d = 0; d < n; d++) {
-					sum [c][d] = (first[c][d] + second [c][d]);
-				}
-			}
-			
-			//	---	---	---	---	---	---	---	---	---
-			
-			System.out.println("Sum of entered matices = ");
-			for(int c = 0; c < m; c++) {
-				for(int d = 0; d < n; d++) {
-					System.out.println(sum [c][d] +"\t");
-				}
-			}
-			System.out.println();
-			
-			//	---	---	---	---	---	---	---	---	---
 		}
+		
+		System.out.println("Enter the elements of your second matrix: ");
+		for(int c = 0; c < m; c++) {
+			for(int d = 0; d < n; d++) {
+				second[c][d] = in.nextInt();
+			}
+		}
+		
+		//	---	---	---	---	---	---	---	---	---
+		
+		for(int c = 0; c < m; c++) {
+			for(int d = 0; d < n; d++) {
+				sum [c][d] = (first[c][d] + second [c][d]);
+			}
+		}
+		
+		//	---	---	---	---	---	---	---	---	---
+		
+		System.out.println("Sum of entered matices = ");
+		for(int c = 0; c < m; c++) {
+			for(int d = 0; d < n; d++) {
+				System.out.println(sum [c][d] +"\t");
+			}
+		}
+		System.out.println();
+		
+		//	---	---	---	---	---	---	---	---	---
 	}
 }
