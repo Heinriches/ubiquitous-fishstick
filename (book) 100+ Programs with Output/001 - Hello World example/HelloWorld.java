283446ebb4da2f3e507a2dc55ea0ad6cd1e44dfc
Heinriches <heydrich.institut@gmail.com>
2025-09-23 11:15:55 -0300

    Update HelloWorld.java

--- (book) 100+ Programs with Output/001 - Hello World example/HelloWorld.java
+++ (book) 100+ Programs with Output/001 - Hello World example/HelloWorld.java
@@ -1,3 +1,10 @@
+/**
+ * 'HelloWorld' is passed as an argument to 'println' method, you can print whatever
+ * you want. There is also a print method which doesn't take the cursor to the 
+ * beggining of the next line as println does. 'System'is a class, 'out' is an object
+ * of the class 'PrintStream' and 'println' is it's method.
+ */
+
 class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello World!");
