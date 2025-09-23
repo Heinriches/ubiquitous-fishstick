4390cfab5a71d181b8beb90c9fb75f064196984d
Heinriches <heydrich.institut@gmail.com>
2025-09-23 12:32:35 -0300

    Update AddTwoMatrix.java

--- (book) 100+ Programs with Output/002 - Add Two Matrices/AddTwoMatrix.java
+++ (book) 100+ Programs with Output/002 - Add Two Matrices/AddTwoMatrix.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 class AddTwoMatrix{
 	public static void main(String[] args) {
-		public static void main(String[]) {
+		public static void main(String[] args) {
 			
 			Scanner in = new Scanner(System.in);
 			
