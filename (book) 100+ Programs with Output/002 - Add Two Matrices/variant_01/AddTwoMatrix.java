253259a367e91bc93776be5bcdd1d8e08ae983e5
Heinriches <heydrich.institut@gmail.com>
2025-09-25 21:25:56 -0300

    Update AddTwoMatrix.java

--- (book) 100+ Programs with Output/002 - Add Two Matrices/variant_01/AddTwoMatrix.java
+++ (book) 100+ Programs with Output/002 - Add Two Matrices/variant_01/AddTwoMatrix.java
@@ -41,18 +41,18 @@ class AddTwoMatrix{
 		//	---	---	---	---	---	---	---	---	---
 		System.out.println("MATRIX 1: ");
 		for(int c = 0 ; c < m ; c++) {
-			System.out.println();
 			for(int d = 0; d < n; d++) {
-				System.out.print(first [c][d] +", " +"\t" );
+				System.out.print(first [c][d] +"\t" );
+				System.out.println();
 			}
 			System.out.println();
 		}
 		
 		System.out.println("MATRIX 2: ");
-		for(int c = 0 ; c < m ; c++) {
-			System.out.println();
+		for(int c = 0 ; c < m ; c++) {			
 			for(int d = 0; d < n; d++) {
-				System.out.print(first [c][d] +", " +"\t" );
+				System.out.print(second [c][d] +"\t" );
+				System.out.println();
 			}
 			System.out.println();
 		}
